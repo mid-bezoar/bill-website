9981c9290a71b3a1703cbd5088016d349f6f8bb5
midbezoar <2974490550@qq.com>
2022-09-04 15:52:27 +0800

    deploy

---
+++ precache-manifest.6e6caf6739e3469c93e8e8142db7e1b1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84f57eb1588cfce5f785",
+    "url": "/bill-website/css/app.40636420.css"
+  },
+  {
+    "revision": "6fcf52b3924b02aabef99554ca20fc9b",
+    "url": "/bill-website/index.html"
+  },
+  {
+    "revision": "84f57eb1588cfce5f785",
+    "url": "/bill-website/js/app.c02eb136.js"
+  },
+  {
+    "revision": "82494fd5d3986b5b5621",
+    "url": "/bill-website/js/chunk-vendors.27ac1c8d.js"
+  },
+  {
+    "revision": "a08f41ddca2233365c3713ba22eb41c2",
+    "url": "/bill-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/bill-website/robots.txt"
+  }
+]);
